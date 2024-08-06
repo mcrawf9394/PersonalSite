d2cdbae63941700e12e3f0e2d0e221564f04eb2a
mcrawf9394 <mjc9394@gmail.com>
2024-08-06 17:33:01 -0400

    Started getting the home page set up to show the technologies that I have used in the past.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,17 @@
+import '../stylesheet/Home.css'
 function Home () {
     return <>
-    
+        <h1 className='homeText'>Sam Crawford</h1>
+        <h2 className='homeText'>Full-Stack Web Developer</h2>
+        <div className='technologies'>
+            <img className='techPics' src="../../javascript.svg" alt="JavaScript" />
+            <img className='techPics' src="../../css-3.svg" alt="CSS" />
+            <img className='techPics' src="../../html-5.svg" alt="HTML" />
+            <img className='techPics' src="../../mongodb.svg" alt="MongoDB" />
+            <img className='techPics' src="../../express.svg" alt="Express" />
+            <img className='techPics' src="../../nodejs.svg" alt="Node" />
+            <img className='techPics' src="../../react.svg" alt="React" />
+        </div>
     </>
 }
 export default Home
