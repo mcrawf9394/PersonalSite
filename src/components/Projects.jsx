5c07d43cf5f1dc71f2a9c972d5b717f989f607a4
mcrawf9394 <mjc9394@gmail.com>
2024-08-07 14:13:20 -0400

    Mostly finished the site, but i do plan on coming back to it, I want to try to make some more projects and then add them and make it look good on all screen types.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,32 @@
 import '../stylesheet/Projects.css'
 function Projects () {
     return <>
-    
+        <div className='projectDisplay'>
+            <a href="https://social-media-app-mcrawf9394.netlify.app" target="_blank" rel="noopener noreferrer">
+                <p>Social Media App</p>
+                <img src="../../Screenshot_20240807_093349.png" alt="Screenshot of Social media app" />
+            </a>
+            <a href="https://messaging-app-mcrawf9394.netlify.app" target="_blank" rel="noopener noreferrer">
+                <p>Messaging App</p>
+                <img src="../../Screenshot_20240807_093428.png" alt="Screenshot of Messaging app" />
+            </a>
+            <a href="https://mcrawf9394.github.io/Battleship/" target="_blank" rel="noopener noreferrer">
+                <p>Battleship</p>
+                <img src="../../Screenshot_20240807_093505.png" alt="Screenshot of Battleship" />
+            </a>
+            <a href="https://shopping-cart-mcrawf9394.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <p>Store App</p>
+                <img src="../../Screenshot_20240807_095842.png" alt="Screenshot of Store app" />
+            </a>
+            <a href="https://resonant-babka-58ff77.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <p>Blog App</p>
+                <img src="../../Screenshot_20240807_095857.png" alt="Screenshot of Blog app" />
+            </a>
+            <a href="https://github.com/mcrawf9394/knightsTravails" target="_blank" rel="noopener noreferrer">
+                <p>Knights Travails</p>
+                <img src="../../Screenshot_20240807_095936.png" alt="Screenshot of github repository" />
+            </a>
+        </div>
     </>
 }
 export default Projects
